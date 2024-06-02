7c0fa4711cd888e51e7d9c9332ab2cb40d2f90de
mindl3ss <justtodesign@gmail.com>
2024-06-02 20:22:21 +0200

    feat: add validation to message model and spec

--- backend/test/models/message_test.rb
+++ backend/test/models/message_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'title should be present' do
+    user = User.create(email: 'test@foo.com')
+    message = user.messages.new(title: 'Message title')
+
+    assert_not message.save, 'Saved message without a title'
+  end
+
+  test 'body should be present' do
+    user = User.create(email: 'test@foo.com')
+    message = user.messages.new(body: 'Message body')
+
+    assert_not message.save, 'Saved message without a body'
+  end
+  
+  test 'should not be valid without a user' do
+    message = Message.new(title: 'Message title', body: 'Message body')
+
+    assert_not message.save, 'Saved message with missing user'
+  end
+
+  test 'should belong to user' do
+    user = User.create(email: 'test@foo.com')
+    message1 = user.messages.create(title: 'message1', body: 'first message')
+
+    assert_respond_to message1, :user
+  end
 end
