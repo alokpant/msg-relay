9021c47c9550440f98f8e8088dca4030076a4490
mindl3ss <justtodesign@gmail.com>
2024-06-02 20:16:06 +0200

    feat: add validation to user model and specs

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,22 @@
 class User < ApplicationRecord
-  has_many :messages
+  has_many :messages, dependent: :destroy
 
-  validates :email, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  before_create :generate_json_web_token
+  before_update :validate_json_web_token_presence
+
+  private
+
+  def generate_json_web_token
+    self.json_web_token = SecureRandom.hex
+  end
+
+  def validate_json_web_token_presence
+    if self.json_web_token.blank?
+      errors.add(:json_web_token, "can't be blank")
+      throw(:abort)
+    end
+  end
 end
--- backend/test/models/user_test.rb
+++ backend/test/models/user_test.rb
@@ -1,7 +1,53 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'email should be present' do
+    user = User.new
+
+    assert_not user.save, 'Saved user without a name'
+  end
+
+  test 'email should have proper format' do
+    user = User.new(email: 'a.b')
+
+    assert_not user.save, 'Saved user with incorrect format'
+  end
+
+  test 'email should be unique and case-insensitive' do
+    existing_user = User.create(email: 'test@foo.com')
+    new_user = User.new(email: 'Test@foo.com')
+
+    assert_not new_user.save, 'Saved user with a non-unique email'
+  end
+
+  test 'json_web_token should be populated automatically' do
+    user = User.create(email: 'test@foo.com')
+
+    assert_not_nil user.json_web_token, 'json_web_token should be generated automatically'
+  end
+
+  test 'should not update json_web_token to an empty field' do
+    user = User.create(email: 'test@foo.com')
+
+    assert_raises(ActiveRecord::RecordNotSaved) do
+      user.update!(json_web_token: "")
+    end
+  end
+
+  test 'should have many message' do
+    user = User.create(email: 'test@foo.com')
+
+    assert_respond_to user, :messages
+  end
+
+  test 'all messages related to user should be destroyed, when user is destroyed' do
+    user = User.create(email: 'test@foo.com')
+
+    message1 = user.messages.create(title: 'message1', body: 'first message')
+    message2 = user.messages.create(title: 'message2', body: 'second message')
+
+    assert_difference('Message.count', -2) do
+      user.destroy
+    end
+  end
 end
